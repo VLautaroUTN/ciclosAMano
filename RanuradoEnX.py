5b2517f455997ed590ed925d96e284bf48ed8ee0
VLautaroUTN <utnlautaro@gmail.com>
2023-08-23 20:21:28 +0200

    --

--- RanuradoEnX.py
+++ RanuradoEnX.py
@@ -1,38 +1,43 @@
 import tkinter
 
 def generar_gcode():
-    etiquetaFinal["text"] = "iniciando"
-    respuesta = open("respuesta.txt", "w")
+    try:
+        etiquetaFinal["text"] = "iniciando"
+        respuesta = open("respuesta.txt", "w")
 
-    x1 = int(valX1.get())
-    Z1 = int(valZ1.get())
-    x2 = int(valX2.get())
-    Z2 = int(valZ2.get())
-    pasos = int(valPasada.get())
+        x1 = int(valX1.get())
+        Z1 = int(valZ1.get())
+        x2 = int(valX2.get())
+        Z2 = int(valZ2.get())
+        pasos = int(valPasada.get())
 
-    resta = x1 - x2
-    if resta % pasos != 0:
-        etiquetaFinal["bg"] = "yellow"
-        etiquetaFinal["text"] = "Los pasoss no son un divisor del diametro a calar"
-    else:
-        vuelta = 1
-        while True:
-            XActual = x1 - (pasos * vuelta)
-            etiquetaFinal["text"] = "Vuelta a X =", XActual
-            respuesta.write("G01 X{0}\n".format(XActual))
-            respuesta.write("G01 Z{0}\n".format(Z2))
-            respuesta.write("G00 x{0} Z{1}\n".format(XActual+1, Z1))
-            vuelta += 1
-            if XActual == x2:
-                etiquetaFinal["bg"] = "green"
-                etiquetaFinal["text"] = "Terminado: respuestas.txt"
-                break
+        resta = x1 - x2
+        if resta % pasos != 0:
+            etiquetaFinal["bg"] = "yellow"
+            etiquetaFinal["text"] = "Los pasoss no son un divisor del diametro a calar"
+        else:
+            vuelta = 1
+            while True:
+                XActual = x1 - (pasos * vuelta)
+                etiquetaFinal["text"] = "Vuelta a X =", XActual
+                respuesta.write("G01 X{0}\n".format(XActual))
+                respuesta.write("G01 Z{0}\n".format(Z2))
+                respuesta.write("G00 x{0} Z{1}\n".format(XActual+1, Z1))
+                vuelta += 1
+                if XActual == x2:
+                    etiquetaFinal["bg"] = "green"
+                    etiquetaFinal["text"] = "Terminado: respuestas.txt"
+                    break
 
-    respuesta.close()
+        respuesta.close()
+    except:
+        etiquetaFinal["bg"] = "red"
+        etiquetaFinal["text"] = "ERROR"
 
 ventana = tkinter.Tk()
-ventana.geometry("550x400")
+ventana.geometry("600x400")
 ventana.configure(bg = "gray")
+ventana.resizable(False, False)
 
 titulo = tkinter.Label(ventana, text = "Generador de ranurados en eje X", font = "Arial 16", bg = "gray")
 textX1 = tkinter.Label(ventana, text = "X-Inicio", bg = "gray", font = "Arial 16")
@@ -41,6 +46,7 @@ textX2 = tkinter.Label(ventana, text = "X-Final", bg = "gray", font = "Arial 16"
 textZ2 = tkinter.Label(ventana, text = "Z-final", bg = "gray", font = "Arial 16")
 textoPaso = tkinter.Label(ventana, text = "mm por pasada", bg = "gray", font = "Arial 16")
 etiquetaFinal = tkinter.Label(ventana, bg = "gray")
+AutorLbl = tkinter.Label(ventana, text="V4ld3z Cristian Lautaro :D" ,bg="white")
 
 valX1 = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
 valZ1 = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
@@ -75,8 +81,11 @@ textoPaso.config(width="13", height="2")
 valPasada.grid(row=5, column=1)
 
 botonCalcular.grid(row=6, column=1)
+
+AutorLbl.grid(row=7, column=0)
+
 etiquetaFinal.grid(row=7,column=1)
-etiquetaFinal.config(height="4")
+etiquetaFinal.config(height="3")
 
 
 
