1b8ca6cfd7e1478804da15b7be2b23e9ffe7e288
VLautaroUTN <utnlautaro@gmail.com>
2023-08-23 20:05:25 +0200

    --

--- RanuradoEnX.py
+++ RanuradoEnX.py
@@ -2,14 +2,13 @@ import tkinter
 
 def generar_gcode():
     print("iniciando")
-    print("...")
     respuesta = open("respuesta.txt", "w")
 
-    x1 = 56#int(input("Valor X1"))
-    Z1 = 0#int(input("valor Z1"))
-    x2 = 10#int(input("valor x2"))
-    Z2 = -8#int(input("valor Z2"))
-    pasos = 2#int(input("Ingrese proifundidad de desvaste"))
+    x1 = int(valX1.get())
+    Z1 = int(valZ1.get())
+    x2 = int(valX2.get())
+    Z2 = int(valZ2.get())
+    pasos = int(valPasada.get())
 
     resta = x1 - x2
     if resta % pasos != 0:
@@ -30,22 +29,53 @@ def generar_gcode():
     respuesta.close()
 
 ventana = tkinter.Tk()
-ventana.geometry("400x300")
+ventana.geometry("550x400")
+ventana.configure(bg = "gray")
 
-etiqueta = tkinter.Label(ventana, text = "Generador de ranurados en eje X", bg = "white", font = "Arial 16")
-etiqueta.pack(side = tkinter.TOP)
+titulo = tkinter.Label(ventana, text = "Generador de ranurados en eje X", font = "Arial 16", bg = "gray")
+textX1 = tkinter.Label(ventana, text = "X-Inicio", bg = "gray", font = "Arial 16")
+textZ1 = tkinter.Label(ventana, text = "Z-Inicio", bg = "gray", font = "Arial 16")
+textX2 = tkinter.Label(ventana, text = "X-Final", bg = "gray", font = "Arial 16")
+textZ2 = tkinter.Label(ventana, text = "Z-final", bg = "gray", font = "Arial 16")
+textoPaso = tkinter.Label(ventana, text = "mm por pasada", bg = "gray", font = "Arial 16")
+final = tkinter.Label(ventana, bg = "gray")
 
-valX1 = tkinter.Entry(ventana, font = "Arial 20", bg = "gray")
-valX1.pack()
-valZ1 = tkinter.Entry(ventana, font = "Arial 20", bg = "gray")
-valZ1.pack()
-valX2 = tkinter.Entry(ventana, font = "Arial 20", bg = "gray")
-valX2.pack()
-valZ2 = tkinter.Entry(ventana, font = "Arial 20", bg = "gray")
-valZ2.pack()
-
+valX1 = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
+valZ1 = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
+valX2 = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
+valZ2 = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
+valPasada = tkinter.Entry(ventana, font = "Arial 20", bg = "white")
 
 botonCalcular = tkinter.Button(ventana, text= "Generar G-code", command = generar_gcode)
-botonCalcular.pack(side = tkinter.BOTTOM)
+
+
+titulo.grid(row = 0, column=1)
+titulo.config(width="30", height="2")
+
+textX1.grid(row = 1, column= 0)
+textX1.config(width="8", height="2")
+valX1.grid(row = 1, column = 1)
+
+textZ1.grid(row = 2, column=0)
+textZ1.config(width="8", height="2")
+valZ1.grid(row=2, column=1)
+
+textX2.grid(row = 3, column= 0)
+textX2.config(width="8", height="2")
+valX2.grid(row = 3, column = 1)
+
+textZ2.grid(row = 4, column=0)
+textZ2.config(width="8", height="2")
+valZ2.grid(row=4, column=1)
+
+textoPaso.grid(row=5, column=0)
+textoPaso.config(width="13", height="2")
+valPasada.grid(row=5, column=1)
+
+botonCalcular.grid(row=6, column=1)
+final.grid(row=7,column=1)
+final.config(width="10", height="2")
+
+
 
 ventana.mainloop()
